491c9147d2350e4880850ea462759cede3e25b35
mamcgahay <144842205+mamcgahay@users.noreply.github.com>
2024-11-14 16:51:22 -0500

    Add files via upload

---
+++ QuestJournal.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+@export var hero: Hero
+@export var icons: Array[Texture2D]
+
+@onready var quest_chooser = $Choser/QuestList
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
